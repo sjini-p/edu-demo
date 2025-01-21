8ac59179541ddb7dac88402106bf269c67130879
sjini-p <jini1426@paran.com>
2025-01-21 22:18:55 +0900

    Update UserRestController.java

--- src/main/java/com/example/demo/controller/UserRestController.java
+++ src/main/java/com/example/demo/controller/UserRestController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 /**
  * packageName    : com.example.demo.controller
